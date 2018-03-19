28e68b48ce30558538d9f9494b545972229056b5
Jake Tran <jake.tranehs@gmail.com>
2018-03-19 01:00:52 -0700

    add scene 1

--- project/source/scenario1.py
+++ project/source/scenario1.py
@@ -1,2 +1,31 @@
-def routeCostFiles(files):
-    with open
+import strings
+
+def read_routes(path):
+    routeDict = {}
+    for line in open(path, 'r'):
+        line = line.split(',')
+        line[1] = line[1].strip()
+        routeDict[line[0]] = line[1]
+    return routeDict
+
+def find_cost(allRoutesCost, phoneNumber):
+    print('\n',allRoutesCost)
+    bestPrice = 0
+    for k, v in allRoutesCost.items():
+        v = float(v)
+        if strings.contains(phoneNumber, k) and bestPrice == 0:
+            bestPrice = v
+        elif strings.contains(phoneNumber, k):
+            if v < bestPrice:
+                bestPrice = v
+    return str(bestPrice)
+
+def write_cost(phoneNumber, price):
+    with open('route-price-1.txt', 'w') as file:
+        file.write(phoneNumber + ' --- ' + price)
+
+if __name__ == '__main__':
+    allRoutes = read_routes('../data/route-costs-4.txt')
+    phoneNumbers = '+15124156620'
+    best_price = find_cost(allRoutes, phoneNumbers)
+    write_cost(phoneNumbers, best_price)
